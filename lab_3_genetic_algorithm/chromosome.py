8357240faab9396a847e1c261ec33636279624b3
skhort <khortiukserhii@ukr.net>
2019-11-12 18:19:29 +0200

    Added base `Chromosome` functionality

--- lab_3_genetic_algorithm/chromosome.py
+++ lab_3_genetic_algorithm/chromosome.py
@@ -1,3 +1,12 @@
 class Chromosome:
-    def __init__(self, value: int):
-        pass
+    @staticmethod
+    def _convert_to_binary(value: int, bin_length: int) -> str:
+        actual_len = len(bin(value)[2:])
+        if actual_len > bin_length:
+            raise ValueError("Binary value greater than allowed `bin_length`")
+
+        return f"{'0' * (bin_length - actual_len)}{bin(value)[2:]}"
+
+    def __init__(self, value: int, bin_length: int):
+        self._bit_value = self._convert_to_binary(value, bin_length)
+        self._value = value
